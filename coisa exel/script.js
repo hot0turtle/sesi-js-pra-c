017dfc1605efc16528382c0709c77d7be3e040fa
hot0turtle <leofarel2015@gmail.com>
2024-03-11 12:28:07 -0300

    alo alert n funfa

---
+++ coisa exel/script.js
@@ -0,0 +1,14 @@
+function AddItem(){
+    var nome = document.getElementById("nome")
+    var valor = document.getElementById("valor")
+    var quantidade = document.getElementById("quantidade")
+
+    if(!nome || !valor || !quantidade){
+        alert("prencha todos os quadrado")  
+        return;
+    }else{
+        console.log("sei la ")
+    }
+
+
+}

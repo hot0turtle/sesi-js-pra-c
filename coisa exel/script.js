3be4d9d881a3cc070378d945b1133c981ffdee43
hot0turtle <leofarel2015@gmail.com>
2024-03-12 09:36:04 -0300

    exel

--- coisa exel/script.js
+++ coisa exel/script.js
@@ -12,5 +12,24 @@ function adicionarItem(){
     //CRIAR A LINHA DA TABELA E PREENCHER COM O VALOR DIGITADO
     //primeira linha da tabela [0]
     var tabela = document.getElementById("tabela").getElementsByTagName("tbody")[0];
+    var novalinha = tabela.insertRow(tabela.rows.length);
 
+    var celulaNome = novalinha.insertCell(0)
+    var celulaValor = novalinha.insertCell(1)
+    var celulaQuantidade = novalinha.insertCell(2)
+
+    celulaNome.innerHTML = nome;
+    celulaValor.innerHTML = valor
+    celulaQuantidade.innerHTML = quantidade
+
+    document.getElementById("nome").value = "";
+    document.getElementById("valor").value = "";
+    document.getElementById("quantidade").value = "";
+}
+
+function exportarParaExcel(){
+    var tabela = document.getElementById ("tabela");
+    var nomeArquivo = "tabela_de_produtos.xlsx";
+    var wb = XLSX.utils.table_to_book(tabela,{sheet: "Tabela de produtos"});
+    XLSX.writeFile(wb, nomeArquivo);
 }
